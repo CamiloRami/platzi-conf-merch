f9d72ff83462eda59c70f2532f0bf466388140cc
Camilo Ramírez <camiloram18042003@gmail.com>
2022-05-12 08:46:14 -0300

    [UPDATE] Header | routes

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,21 +1,26 @@
+import { Link } from 'react-router-dom';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
   return (
     <div className="Checkout">
-      <div className="Chackout-content">
+      <div className="Checkout-content">
         <h3>Lista de Pedidos:</h3>
-        <div className="Chackout-item">
+        <div className="Checkout-item">
           <div className="Checkout-element">
             <h4>ITEM name</h4>
             <span>$10</span>
           </div>
-          <button type="button">Eliminar</button>
+          <button type="button">
+            <i className="fas fa-trash-alt" title="Eliminar" />
+          </button>
         </div>
       </div>
       <div className="Checkout-sidebar">
         <h3>Precio Total: $10</h3>
-        <button type="button">Continuar pedido</button>
+        <Link to="/checkout/information">
+          <button type="button">Continuar pedido</button>
+        </Link>
       </div>
     </div>
   );
